3c631b6f518aeaea67151d1845f4cefe68a6a6ae
Pedro <pedro.freitas@serpro.gov.br>
2024-11-04 10:57:38 -0300

    Aplicação de desconto

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco melância', 5, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = prato('Pãozinho', 2, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
